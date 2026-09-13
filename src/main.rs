13f05cbd900e2edd5f14a3a323089d672eaf959e
Martin <168760158+martin0pl@users.noreply.github.com>
2026-09-13 18:08:45 +0200

    Base for the project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use clap::{Parser, Subcommand};
+
+const DESCRIPTION: &str = "A little tool to help you create new Typst project with a template.";
+
+#[derive(Parser)]
+#[command(name = "croute")]
+#[command(version)]
+#[command(about = DESCRIPTION, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// List all the template avalaible
+    List,
+    /// Create a new project with a template
+    New {
+        /// Title of the template
+        title: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::List => {
+            println!("List");
+        },
+        Commands::New {title} => {
+            println!("List");
+        },
+
+    }
 }
