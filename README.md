5b4108ac0750b3c81e47a1fc7b001f130725e2c0
Martin <168760158+martin0pl@users.noreply.github.com>
2026-09-17 18:45:56 +0200

    Now launch VSCodium when creating a new project

--- README.md
+++ README.md
@@ -1,18 +1,3 @@
 # typsta
 
 A little tool to help you create new Typst project with a template.
-
-## Roadmap
-
-### V0
-
-- [x] data persistence for the templates path in the file ~/.typsta-config.json
-- [x] command `typsta config`
-- [x] command `typsta config <templates_path>`
-- [x] command `typsta list`
-- [x] command `typsta new <project_name> <template_name>`
-- [x] command `typsta update`
-
-### V1
-- [ ] option to choose your favorite text editor (Visual Studio Code or VSCodium)
-- [ ] option to automatically open your text editor on creation (Visual Studio Code or VSCodium)
--- src/commands/new.rs
+++ src/commands/new.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 use std::fs;
+use std::process::Command;
 
 use crate::utils::{expand_tilde,dir_names};
 use crate::app::App;
@@ -21,6 +22,11 @@ pub fn command_new(app: App, template_dir_ok: bool, project_name: String, templa
                     let destination = PathBuf::from(&project_name).join(file);
 
                     let _ = fs::copy(&source, &destination);
+
+                    Command::new("codium")
+                            .arg(format!("./{}/",project_name))
+                            .status()
+                            .expect("Fail to launch VSCodium");
                 }
 
                 println!("New project \"{}\" created successfully with the template \"{}\" !", project_name, template_name);
